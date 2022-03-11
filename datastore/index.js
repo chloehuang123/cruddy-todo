6a86a58589ff79567b56080d066178dd58fa0582
chloe <312864100@qq.com>
2022-03-10 23:45:45 -0500

    working on readAll

--- datastore/index.js
+++ datastore/index.js
@@ -1,37 +1,54 @@
-const fs = require('fs');
-const path = require('path');
-const _ = require('underscore');
-const counter = require('./counter');
+const fs = require("fs");
+const path = require("path");
+const _ = require("underscore");
+const counter = require("./counter");
 
 // var items = {};
 
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-  counter.getNextUniqueId((err, id)=>{
-    if(err) {
+  counter.getNextUniqueId((err, id) => {
+    if (err) {
       callback(err, null);
-    }else {
+    } else {
       //success
       var filePath = `${exports.dataDir}/${id}.txt`;
       fs.writeFile(filePath, text, (err) => {
         if (err) {
           callback(err);
-        }else{
+        } else {
           callback(null, { id, text });
         }
-      })
+      });
     }
   });
   // items[id] = text;
 };
-
 exports.readAll = (callback) => {
-  var data = _.map(items, (text, id) => {
-    return { id, text };
+  fs.readdir(exports.dataDir, (err, files) => {
+    if (err) {
+      throw "error reading file";
+    } else{
+
+      var data = _.map(files, (file) => {
+        var id = file.split(".")[0];
+        // console.log('testing: ', file);
+        var text = fs.readFile(`${exports.dataDir}/${id}.txt`, 'utf8');
+
+        return { id, text };
+      });
+    }
   });
+
   callback(null, data);
 };
+  // Promise.all(data).then(
+  //   (todos) => callback(null, todos),
+  //   (err) => callback(err)
+  // );
+  // var data = _.map(items, (text, id) => {
+  // });
 
 exports.readOne = (id, callback) => {
   var text = items[id];
@@ -65,7 +82,7 @@ exports.delete = (id, callback) => {
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
 
-exports.dataDir = path.join(__dirname, 'data');
+exports.dataDir = path.join(__dirname, "data");
 
 exports.initialize = () => {
   if (!fs.existsSync(exports.dataDir)) {
